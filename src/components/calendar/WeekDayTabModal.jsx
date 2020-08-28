45b0a84f47acf6fa04b12eae7fccac2a6328bec1
Alexandr <Jekyns@yandex.ru>
2020-08-28 22:53:59 +0300

    fixed bug with edit first row

--- src/components/calendar/WeekDayTabModal.jsx
+++ src/components/calendar/WeekDayTabModal.jsx
@@ -5,7 +5,7 @@ import CreateEventsForm from './CreateEventsForm';
 
 function WeekDayTabModal({ dayForEdit,closeModal }) {
   return (
-    <div className={`calendar__wrapper ${dayForEdit.row ? 'modal-open' : 'modal-close'}`}>
+    <div className={`calendar__wrapper ${dayForEdit.row >= 0 ? 'modal-open' : 'modal-close'}`}>
       <div className="calendar__modal">
         <div className="modal__head">
           <p className="modal__head-p">Add Events</p>
