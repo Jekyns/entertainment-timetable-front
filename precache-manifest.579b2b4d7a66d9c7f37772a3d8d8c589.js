7506cccf18e0b234ffdfb5a5b5cf4fca7cf21df7
Alexandr <axkuz97@yandex.ru>
2020-10-06 11:13:22 +0300

    build

---
+++ precache-manifest.579b2b4d7a66d9c7f37772a3d8d8c589.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7ed5dd218e635e70156ef216ab2072",
+    "url": "/entertainment-timetable-front/index.html"
+  },
+  {
+    "revision": "77215dbb425602fdf265",
+    "url": "/entertainment-timetable-front/static/css/main.a6d442b0.chunk.css"
+  },
+  {
+    "revision": "a03360d620fee9468228",
+    "url": "/entertainment-timetable-front/static/js/2.be8d0650.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/entertainment-timetable-front/static/js/2.be8d0650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77215dbb425602fdf265",
+    "url": "/entertainment-timetable-front/static/js/main.2153a421.chunk.js"
+  },
+  {
+    "revision": "07290072f966dffe55b7",
+    "url": "/entertainment-timetable-front/static/js/runtime-main.c0d0c523.js"
+  },
+  {
+    "revision": "29f325ea5279b6e903718486588e5191",
+    "url": "/entertainment-timetable-front/static/media/bg.29f325ea.jpg"
+  },
+  {
+    "revision": "5a226e36b139f8e3415d5de7358db0ed",
+    "url": "/entertainment-timetable-front/static/media/delete.5a226e36.svg"
+  },
+  {
+    "revision": "8eec2a66e32b35a1078dfabfac4461ed",
+    "url": "/entertainment-timetable-front/static/media/elem.8eec2a66.svg"
+  },
+  {
+    "revision": "27ce550f17d3bea73922f7077a1eeb38",
+    "url": "/entertainment-timetable-front/static/media/grid.27ce550f.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/entertainment-timetable-front/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "aa0b5ae2488f49c233cd56f727c64e15",
+    "url": "/entertainment-timetable-front/static/media/pencil.aa0b5ae2.svg"
+  },
+  {
+    "revision": "45882839a716ff5fccfd74306550a880",
+    "url": "/entertainment-timetable-front/static/media/replay.45882839.svg"
+  }
+]);
